61e7e8d8cb173bd0ed3632d3279f2bcb9f9f1e6f
ismailseferaj <68352963+ismailseferaj@users.noreply.github.com>
2020-07-15 22:49:49 +0200

    Shopping Cart

---
+++ projektiFinal/cart.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/home_css.css">
+    <script src="js/home.js">
+
+    </script>
+    <title>Home</title>
+  </head>
+  <body>
+
+
+
+
+  <!--                      H E A D E R                  -->
+    <header class="header">
+    <nav>
+<ul>
+
+<li>
+  <a href="home.html">Home</a>
+</li>
+<li>
+  <a href="store.php" target="_blank">Store</a>
+</li>
+<li>
+  <a href="events.php" target="_blank">Events</a>
+</li>
+<li>
+  <a  href="contactus.php" target="_blank">Contact US</a>
+</li>
+<li>
+
+<a href="cart.php">Chart</a>
+
+</li>
+<li>
+
+<a href="images.html">Images</a>
+
+</li>
+<li id="login">
+<a href="login.php">Login</a>
+</li>
+</ul>
+
+</nav>
+       <div class="header__logo-box">
+         <img src="img/1.png" alt="natours logo" id="home_logo">
+       </div>
+       <div class="text_header">
+         <span class="t1">Discover new Routes</span>
+
+       </div>
+    </header>
+
+
+<!--                           H E A D E R                          -->
+
+
+
+  <main>
+  <h1 align="center" id="eventsh1">Shopping cart comming soon...</h1>
+ 
+
+  </main>
+
+
+
+  <footer class="footer">
+     <div>
+  Have a question? Email us at
+  <a href="mailto:example@example.com">hiking@hike.com</a>
+  </div>
+
+  <div>
+  <a class="social" href="#"><img src="img/fb.png"></a>
+  <a class="social" href="#"><img src="img/ig.png"></a>
+  <a class="social" href="#"><img src="img/tw.png"></a>
+  <a class="social" href="#"><img src="img/yt.png"></a>
+  </div>
+  <div>
+  You'll travel with the wind. <span class="name">HIKE.</span>.
+  </div>
+
+
+  </footer>
+
+
+
+  </body>
+</html>
